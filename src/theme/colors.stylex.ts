b10666a02d8420d3cc4bd6e400b6f5c2efae4c15
Hayeong Jeong <hy78733@gmail.com>
2024-05-09 20:06:10 +0900

    feat: color token

--- src/theme/colors.stylex.ts
+++ src/theme/colors.stylex.ts
@@ -1,6 +1,15 @@
 import * as stylex from '@stylexjs/stylex';
+
 export const colors = stylex.defineVars({
-  primary: 'blue',
-  secondary: 'green',
-  tertiary: 'red',
-});
+  primary100: '#FF5C16',
+  primary200: '#FFE0D3',
+  black: '#111111',
+  white: '#FFFFFF',
+  gray900: '#11111164',
+  gray800: '#11111140',
+  gray700: '#11111112',
+  gray600: '#11111112',
+  bluegray: '#F3F4F6',
+} as const);
+
+export type Color = keyof typeof colors.__tokens;
