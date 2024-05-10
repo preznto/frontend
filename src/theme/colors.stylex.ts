c0386763417ea0062641550fcfabed94b6074c7a
Hayeong Jeong <hy78733@gmail.com>
2024-05-10 11:09:37 +0900

    update: colors

--- src/theme/colors.stylex.ts
+++ src/theme/colors.stylex.ts
@@ -4,12 +4,16 @@ export const colors = stylex.defineVars({
   primary100: '#FF5C16',
   primary200: '#FFE0D3',
   black: '#111111',
+  black64: '#111111A3',
+  black40: '#11111166',
+  black12: '#1111111F',
   white: '#FFFFFF',
-  gray900: '#11111164',
-  gray800: '#11111140',
-  gray700: '#11111112',
-  gray600: '#11111112',
+  white80: '#FFFFFFCC',
+  white64: '#FFFFFFA3',
+  white40: '#FFFFFF66',
+  white8: '#FFFFFF14',
   bluegray: '#F3F4F6',
+  disable: '#DDDDDD',
 } as const);
 
 export type Color = keyof typeof colors.__tokens;
