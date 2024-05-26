6e12da88cf6d75d88abca167a55678291222a893
HaYeong Jeong <hy78733@gmail.com>
2024-05-26 17:48:57 +0900

    [Feature/theme] Feat: color token, typography, global layout (#1)
    * setting: path alias, entry file
    
    * feat: color token
    
    * feat: typography, font, reset css
    
    * feat: global layout
    
    * update: colors
    
    * fix: defined fw is default
    
    * update: colors
    
    * chore: use abs path

---
+++ src/components/common/typography/index.ts
@@ -0,0 +1,10 @@
+import withTypographyBase from './base';
+
+export const Heading1 = withTypographyBase('h1', 'heading1');
+export const Heading2 = withTypographyBase('h2', 'heading2');
+export const Heading3 = withTypographyBase('h3', 'heading3');
+export const Body1 = withTypographyBase('p', 'body1');
+export const Body2 = withTypographyBase('p', 'body2');
+export const Body3 = withTypographyBase('p', 'body3');
+export const Body4 = withTypographyBase('p', 'body4');
+export const Body5 = withTypographyBase('span', 'body5');
