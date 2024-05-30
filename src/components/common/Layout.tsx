65f0cc2ecaa86c4c7e6c72227a4204f1dbf553d4
HaYeong <hy78733@gmail.com>
2024-05-30 20:43:44 +0900

    feat: TopNavigation store, useTopNavigation hook

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,6 +1,5 @@
 import * as stylex from '@stylexjs/stylex';
 import { PropsWithChildren } from 'react';
-import BottomNavigation from './BottomNavigation';
 import { layout } from '../../theme/layout.stylex';
 import TopNavigation from './TopNavigation';
 
@@ -8,7 +7,6 @@ const styles = stylex.create({
   layout: {
     maxWidth: layout.maxWidth,
     paddingBottom: layout.bottomNavigationHeight,
-    paddingTop: layout.topNavigationHeight,
     marginRight: 'auto',
     marginLeft: 'auto',
     height: '100%',
@@ -18,11 +16,8 @@ const styles = stylex.create({
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <main {...stylex.props(styles.layout)}>
-      {/* TODO: 페이지 별로 prop 다르게 주기. 전역 상태로 관리 */}
-      <TopNavigation show={true} left="back" right="close" />
+      <TopNavigation />
       {children}
-      {/* TODO: 페이지 별로 노출 여부 조정하기. 전역 상태로 관리. */}
-      <BottomNavigation show={true} />
     </main>
   );
 };
--- src/components/common/TopNavigation.tsx
+++ src/components/common/TopNavigation.tsx
@@ -1,68 +1,62 @@
-import { ReactElement, useCallback } from 'react';
+import { useCallback } from 'react';
 import { IconClose, IconLeft } from './icons';
 import { useRouter } from '@tanstack/react-router';
 import * as stylex from '@stylexjs/stylex';
 import { layout } from '../../theme/layout.stylex';
 import { colors } from '../../theme/colors.stylex';
-
-interface TopNavigationProps {
-  left: 'back' | 'none' | ReactElement;
-  right?: 'close' | 'none' | ReactElement;
-  show: boolean;
-}
+import useTopNavigationStore from '@/store/topNavigation';
 
 const styles = stylex.create({
-  header: {
-    position: 'fixed',
+  nav: (visible) => ({
+    height: visible ? layout.topNavigationHeight : 0,
+    padding: visible ? '8px 10px' : 0,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     top: 0,
     left: 'auto',
     right: 'auto',
     width: '100%',
     maxWidth: layout.maxWidth,
-    height: layout.topNavigationHeight,
     backgroundColor: colors.white100,
-    padding: '8px 10px',
-  },
-  nav: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
+  }),
   clickable: {
     cursor: 'pointer',
   },
+  space: {
+    marginBottom: layout.topNavigationHeight,
+  },
 });
 
-const TopNavigation: React.FC<TopNavigationProps> = ({ show, left, right = 'none' }) => {
+const TopNavigation: React.FC = () => {
   const router = useRouter();
   const onBack = useCallback(() => router.history.back(), [router.history]);
+  const { visible, left, right } = useTopNavigationStore();
 
   return (
-    show && (
-      <header {...stylex.props(styles.header)}>
-        <nav {...stylex.props(styles.nav)}>
-          <div>
-            {left === 'back' ? (
-              <IconLeft {...stylex.props(styles.clickable)} onClick={onBack} />
-            ) : left === 'none' ? (
-              <div />
-            ) : (
-              left
-            )}
-          </div>
+    <header>
+      <nav {...stylex.props(styles.nav(visible))}>
+        <div>
+          {left === 'back' ? (
+            <IconLeft {...stylex.props(styles.clickable)} onClick={onBack} />
+          ) : left === 'none' ? (
+            <div />
+          ) : (
+            left
+          )}
+        </div>
 
-          <div>
-            {right === 'close' ? (
-              <IconClose {...stylex.props(styles.clickable)} onClick={onBack} />
-            ) : right === 'none' ? (
-              <div />
-            ) : (
-              right
-            )}
-          </div>
-        </nav>
-      </header>
-    )
+        <div>
+          {right === 'close' ? (
+            <IconClose {...stylex.props(styles.clickable)} onClick={onBack} />
+          ) : right === 'none' ? (
+            <div />
+          ) : (
+            right
+          )}
+        </div>
+      </nav>
+    </header>
   );
 };
 
