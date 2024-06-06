320ad45ad85474d4e39c5b4f816652e01db16f8a
Yeom-JinHo <pwlsghq@naver.com>
2024-06-06 20:17:02 +0900

    feat: global color, typo set

---
+++ src/theme/globalTheme.css.ts
@@ -0,0 +1,28 @@
+import { createGlobalTheme } from '@vanilla-extract/css';
+
+export const global = createGlobalTheme(':root', {
+  color: {
+    black100: '#111111',
+    black80: '#111111CC',
+    black64: '#111111A3',
+    black40: '#11111166',
+    black20: '#11111133',
+    black12: '#1111111F',
+    black08: '#11111114',
+    black04: '#1111110A',
+    white100: '#FFFFFF',
+    white80: '#FFFFFFCC',
+    white64: '#FFFFFFA3',
+    white40: '#FFFFFF66',
+    white20: '#FFFFFF33',
+    white12: '#FFFFFF1F',
+    white08: '#FFFFFF14',
+    white04: '#FFFFFF0A',
+    grayblue: '#FAFBFB',
+    gray08: '#ECECEC',
+    gray04: '#F5F5F5',
+    red: '#FF0000',
+    secondary100: '#0075FF',
+    secondary200: '#EBF4FF',
+  },
+});
---
+++ src/theme/typography.css.ts
@@ -0,0 +1,44 @@
+import { styleVariants } from '@vanilla-extract/css';
+
+export const typography = styleVariants({
+  heading1: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    lineHeight: '36px',
+  },
+  heading2: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    lineHeight: '32px',
+  },
+  heading3: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    lineHeight: '30px',
+  },
+  body1: {
+    fontSize: 18,
+    fontWeight: 'normal',
+    lineHeight: '26px',
+  },
+  body2: {
+    fontSize: 16,
+    fontWeight: 'normal',
+    lineHeight: '24px',
+  },
+  body3: {
+    fontSize: 14,
+    fontWeight: 'normal',
+    lineHeight: '22px',
+  },
+  body4: {
+    fontSize: 12,
+    fontWeight: 'normal',
+    lineHeight: '18px',
+  },
+  body5: {
+    fontSize: 10,
+    fontWeight: 'normal',
+    lineHeight: '11px',
+  },
+});
