07c695a424191138e33dcbc0b12f98a17acc3829
Yeom-JinHo <pwlsghq@naver.com>
2024-06-07 15:41:03 +0900

    fix: global import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import '@/App.css';
+import './theme/globalTheme.css'
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
