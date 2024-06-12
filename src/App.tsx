6653a8dae82d0f9c4201a05ea55cac3e61ac92ab
Yeom-JinHo <pwlsghq@naver.com>
2024-06-12 21:22:19 +0900

    refactor: 절대경로로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import '@/App.css';
-import './theme/globalTheme.css'
+import '@/theme/globalTheme.css'
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
