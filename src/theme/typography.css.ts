74c708d7a521a98052b446a718cbff682c59e727
Yeom-JinHo <pwlsghq@naver.com>
2024-06-12 21:23:08 +0900

    refactor: typo px단위 통일(제거)

--- src/theme/typography.css.ts
+++ src/theme/typography.css.ts
@@ -4,41 +4,41 @@ export const typography = styleVariants({
   heading1: {
     fontSize: 28,
     fontWeight: 'bold',
-    lineHeight: '36px',
+    lineHeight: 36,
   },
   heading2: {
     fontSize: 24,
     fontWeight: 'bold',
-    lineHeight: '32px',
+    lineHeight: 32,
   },
   heading3: {
     fontSize: 20,
     fontWeight: 'bold',
-    lineHeight: '30px',
+    lineHeight: 30,
   },
   body1: {
     fontSize: 18,
     fontWeight: 'normal',
-    lineHeight: '26px',
+    lineHeight: 26,
   },
   body2: {
     fontSize: 16,
     fontWeight: 'normal',
-    lineHeight: '24px',
+    lineHeight: 24,
   },
   body3: {
     fontSize: 14,
     fontWeight: 'normal',
-    lineHeight: '22px',
+    lineHeight: 22,
   },
   body4: {
     fontSize: 12,
     fontWeight: 'normal',
-    lineHeight: '18px',
+    lineHeight: 18,
   },
   body5: {
     fontSize: 10,
     fontWeight: 'normal',
-    lineHeight: '11px',
+    lineHeight: 11,
   },
 });
