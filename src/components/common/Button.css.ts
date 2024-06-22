1eaf3cc7db5329901b14400ed8d3423647487176
Yeom-JinHo <81306489+Yeom-JinHo@users.noreply.github.com>
2024-06-22 17:10:20 +0900

    [Feature/button] Button, TextButton (#6)
    * chore: lottie-player 추가
    
    * feat: Button(일반버튼) 구현
    
    * feat: Plus Icon 추가
    
    * refactor: px 제거
    
    * fix: pressedButton Color
    
    * feat: TextButton 구현
    
    * refactor: vars 공통 컬러 사용
    
    Co-authored-by: Izhe Shin <xynezae@gmail.com>
    
    * refactor: vars 공통 컬러 사용
    
    Co-authored-by: Izhe Shin <xynezae@gmail.com>
    
    * feat: button size default 제거
    
    * refactor: 컴포 React.FC 형식으로 통일
    
    * refactor: themecolor -> color로 통합
    
    * feat: is 접두사 제거
    
    ---------
    
    Co-authored-by: Izhe Shin <xynezae@gmail.com>

---
+++ src/components/common/Button.css.ts
@@ -0,0 +1,99 @@
+import { vars } from '@/theme/globalTheme.css';
+import { recipe } from '@vanilla-extract/recipes';
+
+const pressedButtonStyle = (color: string) => ({
+  selectors: {
+    '&:hover:not(:disabled)': {
+      color: color,
+    },
+    '&:active:not(:disabled)': {
+      color: color,
+    },
+    '&:focus:not(:disabled)': {
+      color: color,
+    },
+  },
+});
+
+export const buttonStyle = recipe({
+  base: [
+    {
+      borderWidth: 0,
+      borderRadius: 100,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: vars.color.white100,
+      width: '100%',
+      marginBottom: 24,
+
+      ':disabled': {
+        backgroundColor: vars.color.black08,
+        color: vars.color.black40,
+      },
+    },
+    pressedButtonStyle(vars.color.white64),
+  ],
+  variants: {
+    type: {
+      primary: {
+        backgroundColor: vars.color.black100,
+      },
+      secondary: {
+        backgroundColor: vars.color.secondary100,
+      },
+      gray: [
+        {
+          backgroundColor: vars.color.black12,
+          color: vars.color.black100,
+        },
+        pressedButtonStyle(vars.color.black64),
+      ],
+    },
+    size: {
+      large: [
+        {
+          height: 56,
+        },
+      ],
+      medium: [
+        {
+          width: 57,
+          height: 40,
+        },
+      ],
+      small: [
+        {
+          width: 49,
+          height: 32,
+        },
+      ],
+    },
+  },
+});
+
+export const spinner = recipe({
+  // TODO: 로딩시에 button width 확인 필요
+  variants: {
+    size: {
+      large: [
+        {
+          height: 32,
+          width: 32,
+        },
+      ],
+      medium: [
+        {
+          width: 24,
+          height: 24,
+        },
+      ],
+      small: [
+        {
+          width: 24,
+          height: 24,
+        },
+      ],
+    },
+  },
+});
--- src/components/common/icons/Left.tsx
+++ src/components/common/icons/Left.tsx
@@ -2,7 +2,7 @@ import type { SVGProps } from 'react';
 const SvgLeft = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} fill="none" {...props}>
     <path
-      stroke="#111"
+      stroke="current"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={2.5}
