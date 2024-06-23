abcfa196c36c8444a517e1b591c7a2c1ea65d8c2
Yeom-JinHo <pwlsghq@naver.com>
2024-06-23 20:01:50 +0900

    feat: 위시리스트 상세페이지 이미지영역 스타일링

---
+++ src/pages/my/MyWishItemDetailPage.css.ts
@@ -0,0 +1,18 @@
+import { style } from '@vanilla-extract/css';
+
+const wishListInfoStyle = style({});
+
+export const wishItemImageSection = style({
+  position: 'relative',
+});
+
+export const wishItemImg = style({
+  width: '100%',
+  height: 393,
+});
+
+export const linkBtn = style({
+  position: 'absolute',
+  bottom: -2,
+  left: 20,
+});
--- src/pages/my/MyWishItemDetailPage.tsx
+++ src/pages/my/MyWishItemDetailPage.tsx
@@ -2,7 +2,34 @@ import Badge from '@/components/common/Badge';
 import TopNavigation from '@/components/common/TopNavigation';
 import { Body12, Body14, Body16, Body18, Heading24 } from '@/components/common/typography';
 import useTopNavigation from '@/hooks/useTopNavigation';
-import React, { Fragment } from 'react';
+import React, { Fragment, SyntheticEvent } from 'react';
+import { linkBtn, wishItemImageSection, wishItemImg } from './MyWishItemDetailPage.css';
+import Button from '@/components/common/Button';
+import defaultItemImg from '@/assets/defaultItemImg.png';
+
+const WishItemImage = () => {
+  const handleImageError = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = defaultItemImg;
+  };
+
+  const handleLinkBtnClick = () => {
+    window.open('https://www.naver.com');
+  };
+
+  return (
+    <section className={wishItemImageSection}>
+      <img
+        className={wishItemImg}
+        src="https://picsum.photos/id/237/1000/1000"
+        alt="상품 이미지"
+        onError={handleImageError}
+      />
+      <Button className={linkBtn} type="primary" size="small" onClick={handleLinkBtnClick}>
+        링크 열기
+      </Button>
+    </section>
+  );
+};
 
 const WishListInfo = () => {
   return (
