219e09d0002b1f71f0a1261ceea5e58816e94b23
eazae <xynezae@gmail.com>
2024-06-29 21:16:37 +0900

    chore: clean unused code

--- src/components/common/TextField.css.ts
+++ src/components/common/TextField.css.ts
@@ -1,4 +1,4 @@
-import { style, createThemeContract, createTheme, styleVariants } from '@vanilla-extract/css';
+import { style, styleVariants } from '@vanilla-extract/css';
 import { typography } from '@/theme/typography.css';
 import { colors } from '@/theme/globalTheme.css';
 
@@ -14,7 +14,6 @@ export const inputWrapper = style({
   width: 'fit-content',
 });
 
-// const [inputBase, vars] = createTheme([
 export const inputBase = style([
   typography.body16,
   {
@@ -27,88 +26,35 @@ export const inputBase = style([
     outlineOffset: '-1px',
     borderColor: 'transparent',
     padding: '16px 20px',
-    caretColor: colors.secondary100, // TODO colors - but do I want this?
+    caretColor: colors.secondary100,
     caretShape: 'bar',
     '::-webkit-outer-spin-button': {
-      // '-webkit-appearance': 'none',
       display: 'none',
       margin: 0,
       WebkitAppearance: 'none',
       MozAppearance: 'none',
       appearance: 'none',
     },
-    // '-moz-appearance': 'textfield',
     '::placeholder': {
       color: colors.black64,
-      // TODO typo body14
-      fontSize: 14,
-      // placeholderText,
+      fontSize: typography.body12,
     },
     ':focus': {
-      // outline: 'none',
       backgroundColor: colors.secondary200,
     },
     ':read-only': {
       backgroundColor: colors.black08,
       color: colors.black100,
       cursor: 'default',
-      // appearance: 'textfield',
-      // '-moz-appearance': button, /* Firefox */
-      // WebkitAppearance: 'textfield' /* Safari and Chrome */,
     },
     ':disabled': {
       outlineColor: colors.black04,
-      // color: colors.black40,
       backgroundColor: colors.black08,
       color: colors.black40,
     },
-    // ':invalid': {
-    //   outline: '1px solid',
-    //   outlineColor: 'red', // TODO colors
-    // },
   },
 ]);
 
-// TODO vhs font color -> type 변환
-export const input = styleVariants({
-  default: [inputBase],
-  // focus: [
-  //   inputBase,
-  //   {
-  //     outlineColor: colors.black04,
-  //     color: colors.red,
-  //     backgroundColor: colors.black64,
-  //   },
-  // ],
-  error: [
-    inputBase,
-    {
-      // outlineColor: colors.black04,
-      outlineColor: colors.red,
-      color: colors.red,
-      // backgroundColor: colors.red,
-    },
-  ],
-  disabled: [
-    inputBase,
-    {
-      outlineColor: colors.black04,
-      color: colors.black40,
-      // backgroundColor: colors.black40,
-    },
-  ],
-  readonly: [
-    inputBase,
-    {
-      outlineColor: colors.black04,
-      color: colors.red,
-      // backgroundColor: colors.black40,
-    },
-  ],
-});
-
-// export const emptyInput = style([inputBase, {}]);
-// export const focusInput = style([inputBase, {}]);
 export const inputError = style([
   inputBase,
   {
@@ -118,23 +64,6 @@ export const inputError = style([
     },
   },
 ]);
-// export const readonlyInput = style([inputBase, {}]);
-
-// export const label = style([
-//   typography.body14,
-//   {
-//     color: colors.black64,
-//     marginBottom: '4px',
-//   },
-// ]);
-
-// export const [label, vars] = createTheme({
-//   color: {
-//     default: colors.black64,
-//     error: colors.red,
-//     disabled: colors.black40,
-//   },
-// });
 
 const labelBase = style([
   typography.body14,
@@ -162,14 +91,6 @@ export const helperText = styleVariants({
   disabled: [helperTextBase, { color: colors.black40 }],
 });
 
-// export const errorText = style([
-//   typography.body12,
-//   {
-//     color: colors.red,
-//     marginTop: '4px',
-//   },
-// ]);
-
 export const unit = style({
   position: 'absolute',
   display: 'inline-flex',
@@ -191,7 +112,4 @@ export const clearBtn = style({
   ':active': {
     transform: 'scale(0.9)', // TODO 임의
   },
-  // ':visited': {
-  // transform: 'scale(1)', // TODO 임의
-  // },
 });
