67e1c7df75fa8bebdb39b1987e854b7a7547f0dc
Yeom-JinHo <pwlsghq@naver.com>
2024-07-07 17:35:38 +0900

    fix: route 경로명 수정

--- src/routes/my/wishlist.$wishlistId.items.index.tsx
+++ src/routes/my/wishlist.$wishlistId.items.index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-export const Route = createFileRoute('/my/wishlist/$wishlistId/items')({
+export const Route = createFileRoute('/my/wishlist/$wishlistId/items/')({
   component: () => <div>Hello /my/wishlist/!</div>,
 });
