7a3faa2e385835bbc467c029d3874745ea389b1c
Yeom-JinHo <pwlsghq@naver.com>
2024-07-07 17:44:43 +0900

    feat: 화이트 모드 설정으로 변경

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,12 +1,12 @@
 import { PropsWithChildren } from 'react';
-import { darkTheme } from '@/theme/globalTheme.css';
+import { lightTheme } from '@/theme/globalTheme.css';
 import { layout } from './Layout.css';
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   // 다크/라이트 모드 추가시 주석해제
   // const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-  return <main className={`${layout} ${darkTheme}`}>{children}</main>;
+  return <main className={`${layout} ${lightTheme}`}>{children}</main>;
 };
 
 export default Layout;
